4aa2ee9a6c70df08709fa745d3133de141848292
LeSafsof <60992151+Safsofx@users.noreply.github.com>
2025-03-26 21:53:45 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const crops = [];
+
+function plantCrop() {
+    const crop = {
+        id: crops.length + 1,
+        name: 'Wheat',
+        plantedAt: Date.now(),
+        harvestTime: 30000 // 30 seconds for testing
+    };
+    crops.push(crop);
+    updateUI();
+}
+
+function updateUI() {
+    const cropList = document.getElementById('crops');
+    cropList.innerHTML = '';
+    crops.forEach(crop => {
+        const remainingTime = (crop.plantedAt + crop.harvestTime - Date.now()) / 1000;
+        cropList.innerHTML += `<p>${crop.name} - Harvest in: ${remainingTime.toFixed(1)}s</p>`;
+    });
+}
+
+setInterval(updateUI, 1000);
+
+function connectPiNetwork() {
+    console.log('Connecting to Pi Network...');
+    // TODO: Add Pi Testnet authentication and transactions here
+}
